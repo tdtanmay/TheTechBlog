4ef88a0da6f6c692993f4a15dda37ec446c76ed9
tdtanmay <tdtanmay8@gmail.com>
2017-11-16 01:31:44 +0530

    The Tech Blog, first commit

---
+++ myblog/blog/admin.py
@@ -0,0 +1,10 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+from blog.models import Post,Comment
+
+
+from django.contrib import admin
+
+# Register your models here.
+admin.site.register(Post)
+admin.site.register(Comment)
